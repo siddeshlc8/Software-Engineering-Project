404aeb14fa5bf860223c341424378779d1063717
Siddesh LC <siddeshlc8@gmail.com>
2018-02-17 15:28:41 +0530

    players app updated

--- player/models.py
+++ player/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Player(User):
+
+    PLAYER_TYPE_CHOICES = [
+        ('Batsman', 'Batsman'),
+        ('Bowler', 'Bowler'),
+        ('All_Rounder', 'All_Rounder')
+    ]
+    player_type = models.CharField(max_length=11, choices=PLAYER_TYPE_CHOICES)
+    phone_no = models.CharField(max_length=10)
+    nationality = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    district = models.CharField(max_length=20)
+    dob = models.DateField()
+
+
+
--- player/views.py
+++ player/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import PlayerSignUpForm
+from .models import Player
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = PlayerSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'player/registration/signup.html')
+    else:
+        form = PlayerSignUpForm()
+    return render(request, 'player/registration/signup.html', {'form': form})
+
