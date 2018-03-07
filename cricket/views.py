88bb688919e349fb4c8e2d2c6d11260f5f8ca4e1
shreyas <shreyaspandith1210@gmail.com>
2018-03-07 22:29:28 +0530

    organizer navbar updated

--- cricket/views.py
+++ cricket/views.py
@@ -36,8 +36,9 @@ def signin(request):
                     Organizer.objects.get(pk=request.user)
                     return redirect('organizer:home')
                 except Exception:
-                     return redirect('signin')
-    return render(request, 'cricket/login.html')
+                        return redirect('cricket:login')
+    else:
+       return render(request, 'cricket/login.html')
 
 
 def player_signup(request):
