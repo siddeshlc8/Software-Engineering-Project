7718a67ec2ca1e47a99f686b84bc722c7fb1629a
Shreyas <shreyaspandith1210@gmail.com>
2018-03-13 14:33:49 +0530

    updated

--- alerts/urls.py
+++ alerts/urls.py
@@ -5,6 +5,10 @@ from alerts import views
 
 app_name = 'alerts'
 urlpatterns=[
+
+
+
+
     path('',views.tournament_alerts,name='tournament_alerts'),
     path('create_alert/',views.create_tournament_alert,name='create_tournament_alert'),
 
